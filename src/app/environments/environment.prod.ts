ec133fcee2b8602c505254ef69856e35147b54e3
aklilufregzi <aklilu2424@gmail.com>
2023-03-24 09:34:18 +0300

    UI has been made responsive

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBBhrunAevuh5oXPBwrAGUPgZdvBIitQIA",
+        authDomain: "zuns-buisness.firebaseapp.com",
+        projectId: "zuns-buisness",
+        storageBucket: "zuns-buisness.appspot.com",
+        messagingSenderId: "92813527544",
+        appId: "1:92813527544:web:100a749dc73ed3546f2e0d"
+    }
+}
--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  showMobileNav: boolean = false;
 
+  toggleNav() {
+    this.showMobileNav = !this.showMobileNav;
+  }
 }
